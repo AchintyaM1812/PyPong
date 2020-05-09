83a759f518e13245948236b8530130b10d7a0610
Achintya Mathur <iamachintya18@gmail.com>
2020-05-09 18:39:29 +0530

    added bouncing ball

--- src/main.py
+++ src/main.py
@@ -4,17 +4,15 @@
 
 import pygame
 from pygame.locals import *
-from players import Paddle
-from ball import Ball
 import sys
 
 pygame.init()
 clock = pygame.time.Clock()
 
-width = 700
-height = 600
-dimensions = (width, height)
-frameRate = 30
+window_width = 700
+window_height = 600
+dimensions = (window_width, window_height)
+frameRate = 35
 
 red = (255, 0, 0)
 green = (0, 255, 0)
@@ -25,18 +23,47 @@ window = pygame.display.set_mode(dimensions)
 pygame.display.set_caption("Pong")
 
 
+class Paddle():
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.velocity = 7
+
+    def draw(self, window):
+        pygame.draw.rect(window, self.color,
+                         (self.x, self.y, self.width, self.height))
+
+    def moveUp(self):
+        self.y -= self.velocity
+        if self.y <= 0:
+            self.y = 0
+
+    def moveDown(self):
+        self.y += self.velocity
+        if self.y + self.height >= window_height:
+            self.y = window_height - self.height
+
+
+ball = pygame.Rect(window_width / 2 - 10, window_height / 2 - 10, 20, 20)
+ball_x_velocity = 8
+ball_y_velocity = 8
+player = Paddle(10, window_height / 2 - 50, 25, 100, green)
+opponent = Paddle(window_width - 35, window_height / 2 - 50, 25, 100, green)
+
+
 def refresh():
     window.fill(black)
-    pygame.draw.line(window, white, (width / 2, 0), (width / 2, height), 3)
+    pygame.draw.line(window, white, (window_width / 2, 0),
+                     (window_width / 2, window_height), 3)
+    pygame.draw.ellipse(window, red, ball)
     player.draw(window)
     opponent.draw(window)
-    ball.draw(window)
-    ball.move()
+    ball.x += ball_x_velocity
+    ball.y += ball_y_velocity
 
-
-player = Paddle(10, height / 2 - 50, 25, 100, green)
-opponent = Paddle(width - 35, height / 2 - 50, 25, 100, green)
-ball = Ball(width / 2, height / 2, 20, 20, red)
 
 running = True
 while running:
@@ -46,6 +73,12 @@ while running:
     events = pygame.event.get()
     pygame.event.pump()
     keys = pygame.key.get_pressed()
+
+    if ball.top <= 0 or ball.bottom >= window_height:
+        ball_y_velocity *= -1
+
+    if ball.left <= 0 or ball.right >= window_width:
+        ball_x_velocity *= -1
 
     for event in events:
         if event.type == pygame.QUIT:
